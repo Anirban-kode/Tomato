8f7b53d9280f2b0f4f47feb1bc7d2a5540524da9
Anirban Mukherjee <anirbanmukherjee0987@gmail.com>
2026-05-24 13:22:31 +0530

    done with Addcart css property

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
 
-    const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className='cart'>
@@ -18,12 +18,19 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cartItems[item._id]>0)
-          {
-            return(
-              <div className='cart-items-title cart-items-item'>
-                <p>{item.name}</p>
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className='cart-items-title cart-items-item'>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{item.price * cartItems[item._id]}</p>
+                  <p className='cross'>x</p>
+                </div>
+                <hr />
               </div>
             )
           }
