dc933ea1ccf0a368de10a3eeb766be9953471643
Anirban Mukherjee <anirbanmukherjee0987@gmail.com>
2026-06-13 16:43:55 +0530

    Done 2nd part

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './MyOrders.css'
+import { StoreContext } from '../../context/StoreContext'
+import axios from 'axios';
 
 const MyOrders = () => {
+
+  const {url,token} = useContext(StoreContext);
+  const [data,setData] = useState([]);
+
+  const fetchOrders = async () => {
+    const response = await axios.post(url+"/api/order/userorders",{},{headers:{token}});
+    setData(response.data.data);
+  }
+
   return (
     <div>
       
