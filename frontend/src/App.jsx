9472fc84c751e413744c02e519aba883d3a8ad53
Anirban Mukherjee <anirbanmukherjee0987@gmail.com>
2026-06-13 16:29:08 +0530

    Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from './pages/Verify/Verify'
+import MyOrders from './pages/MyOrders/MyOrders'
 
 const App = () => {
 
@@ -22,6 +23,7 @@ const App = () => {
           <Route path='/Cart' element={<Cart />} />
           <Route path='/Order' element={<PlaceOrder />} />
           <Route path='/Verify' element={<Verify/>}/>
+          <Route path='/myorders' element={<MyOrders/>}/>
         </Routes>
       </div>
       <Footer />
